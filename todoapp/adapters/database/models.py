81197db3b22e22faf466dedc3266c90d59a08146
David Alvarez <davalv@televes.com>
2025-08-26 12:39:03 +0200

    feat: todo has a user owner

--- todoapp/adapters/database/models.py
+++ todoapp/adapters/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import VARCHAR, Boolean, Integer, String
+from sqlalchemy import Boolean, ForeignKey, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from todoapp.domain.models.user import UserRole
@@ -11,11 +11,11 @@ class BaseORM(DeclarativeBase):
 class UsersORM(BaseORM):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
-    email: Mapped[str] = mapped_column(String, unique=True)
-    username: Mapped[str] = mapped_column(String, unique=True)
-    password: Mapped[str] = mapped_column(String)
-    salt: Mapped[str] = mapped_column(VARCHAR(255))
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(320), unique=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    password: Mapped[str] = mapped_column(String(255))
+    salt: Mapped[str] = mapped_column(String(255))
     role: Mapped[int] = mapped_column(Integer, default=UserRole.NORMAL.value)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
@@ -23,10 +23,9 @@ class UsersORM(BaseORM):
 class TodosORM(BaseORM):
     __tablename__ = "todos"
 
-    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
-    title: Mapped[str] = mapped_column(String)
-    description: Mapped[str] = mapped_column(String)
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(1024))
     priority: Mapped[int] = mapped_column(Integer)
     completed: Mapped[bool] = mapped_column(Boolean, default=False)
-    # owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    owner_id: Mapped[int] = mapped_column(Integer)
+    owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
