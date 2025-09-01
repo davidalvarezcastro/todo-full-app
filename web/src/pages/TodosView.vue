c6c3908d0d00038217edc34d3d8773fe888ebbc1
David Alvarez <davalv@televes.com>
2025-09-01 13:47:24 +0200

    feat(web): delete button

--- web/src/pages/TodosView.vue
+++ web/src/pages/TodosView.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
+import { useDeleteTodo } from '@/composables/mutations/useDeleteTodo'
 import { useTodosQuery } from '@/composables/queries/useTodosQuery'
 import { ref, computed } from 'vue'
 
 const page = ref(1)
 const itemsPerPage = ref(12)
 
-// Llamada a la query
+// Query & Mutations
 const { data, isPending, isError, error } = useTodosQuery({
   page: page,
   items: itemsPerPage,
 })
 
-// Paginación
+const deleteTodoMutation = useDeleteTodo()
+
+// Pagination
 const totalPages = computed(() => {
   return data.value?.total_items ? Math.ceil(data.value.total_items / itemsPerPage.value) : 1
 })
@@ -25,6 +28,12 @@ const nextPage = () => {
 const prevPage = () => {
   if (page.value > 1) {
     page.value--
+  }
+}
+
+const handleDelete = (id: string) => {
+  if (confirm('Are you sure you want to delete this todo?')) {
+    deleteTodoMutation.mutate({ todo: id })
   }
 }
 </script>
@@ -52,13 +61,14 @@ const prevPage = () => {
                 <th>Title</th>
                 <th>Description</th>
                 <th>Status</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               <tr
                 v-for="(todo, index) in data.todos"
                 :key="todo.id"
-                :class="todo.completed ? 'bg-success/20' : 'bg-warning/20'"
+                :class="todo.completed ? 'bg-success/25' : 'bg-warning/5'"
               >
                 <td>{{ index + 1 + (page - 1) * itemsPerPage }}</td>
                 <td>{{ todo.title }}</td>
@@ -89,6 +99,34 @@ const prevPage = () => {
                       <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" />
                     </svg>
                   </span>
+                </td>
+
+                <!-- Actions column -->
+                <td>
+                  <button
+                    class="btn btn-square btn-error btn-sm"
+                    :disabled="deleteTodoMutation.isPending.value"
+                    @click="handleDelete(todo.id)"
+                  >
+                    <svg
+                      v-if="!deleteTodoMutation.isPending.value"
+                      class="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+
+                    <span v-else class="loading loading-spinner"></span>
+                  </button>
                 </td>
               </tr>
             </tbody>
