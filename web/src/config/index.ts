d47fc2d280f46962845a41cef78863f0964775cc
David Alvarez <davalv@televes.com>
2025-09-02 07:53:36 +0200

    refactor(web): build API_HOST from env vars

--- web/src/config/index.ts
+++ web/src/config/index.ts
@@ -1,7 +1,12 @@
+const BASE_URL = import.meta.env.VITE_API_HOSTNAME || window.location.hostname
+const BASE_PORT = import.meta.env.VITE_API_PORT || 5000
+
+const API_HOST = `${window.location.protocol}//${BASE_URL}:${BASE_PORT}`
+
 interface AppConfig {
   API_HOST: string
 }
 
 export const config: AppConfig = {
-  API_HOST: import.meta.env.VITE_API_HOST,
+  API_HOST: API_HOST,
 }
